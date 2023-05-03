bdb9dd42941943ece37f2418fd234c42e1fd3654
ouseqqam <ouseqqam@student.1337.ma>
2023-05-03 18:01:19 +0100

    add useRef

--- components/login/index.jsx
+++ components/login/index.jsx
@@ -1,6 +1,16 @@
+import { useRef } from 'react'
 import styles from './login.module.css'
 
 export function Login () {
+    const username = useRef()
+    const password = useRef()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(username.current.value)
+        console.log(password.current.value)
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.logo}>
@@ -8,17 +18,17 @@ export function Login () {
             </div>
             <div className={styles.form} >
                 <h1>Login</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className={styles.input}>
                         <label htmlFor="username">username</label>
-                        <input type="text" placeholder="username" />
+                        <input type="text" placeholder="username" ref={username} />
                     </div>
                     <div className={styles.input}>
                         <label htmlFor="password">Password</label>
-                        <input type="text" placeholder="password" />
+                        <input type="text" placeholder="password" ref={password} />
                     </div>
                     <div className={styles.btn}>
-                        <button>Login</button>
+                        <button type='submit' >Login</button>
                     </div>
                 </form>
             </div>
